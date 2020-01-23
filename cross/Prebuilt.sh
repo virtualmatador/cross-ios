c74dc2dca30c783424c360a485a0b0556a26e681
Ali Asadpoor <virtualmatador@yahoo.com>
2020-01-22 21:15:40 -0500

    Support iPad

--- cross/Prebuilt.sh
+++ cross/Prebuilt.sh
@@ -23,12 +23,18 @@ then
 fi
 echo "main.cpp generated."
 
+sips --resampleWidth 20 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-20@1.png
 sips --resampleWidth 40 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-20@2.png
 sips --resampleWidth 60 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-20@3.png
+sips --resampleWidth 29 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-29@1.png
 sips --resampleWidth 58 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-29@2.png
 sips --resampleWidth 87 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-29@3.png
+sips --resampleWidth 40 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-40@1.png
 sips --resampleWidth 80 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-40@2.png
 sips --resampleWidth 120 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-40@3.png
 sips --resampleWidth 120 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-60@2.png
 sips --resampleWidth 180 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-60@3.png
+sips --resampleWidth 76 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-76@1.png
+sips --resampleWidth 152 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-76@2.png
+sips --resampleWidth 167 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-83.5@2.png
 sips --resampleWidth 1024 ../../assets/icon.png --out cross/Assets.xcassets/AppIcon.appiconset/icon-1024@1.png
